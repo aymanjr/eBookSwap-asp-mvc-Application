0850d196f4fa8d3665999c9aa1d2963e554d5d65
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-11-08 17:52:12 +0100

    Add project files.

---
+++ ebookSwapApllication/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ebookSwapApllication.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string BookTitle { get; set; } = String.Empty;
+        public string BookISBN13 { get; set; } = String.Empty;
+        public string BookLanguage { get; set; } = String.Empty;
+        public string BookNumPages { get; set; } = String.Empty;
+        public DateTime BookPublicationDate { get; set; } 
+        public string BookPublisherID { get; set; } = String.Empty;
+        public string BookAuthor { get; set; } = String.Empty;
+        public string BookCategory { get; set; } = String.Empty;
+        public string BookDescription { get; set; } = String.Empty;
+        public string BookCondition { get; set; } = String.Empty;
+        public string BookNotes { get; set; } = String.Empty;
+        public string BookImagePath { get; set; } = String.Empty;
+
+
+    }
+}
---
+++ ebookSwapApllication/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace ebookSwapApllication.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; } = String.Empty;
+        public string UserFullName { get; set; } = String.Empty;
+        public string UserEmail { get; set; } = String.Empty;
+        public string UserPassword { get; set; } = String.Empty;
+        public string Userphonenumber { get; set; } = String.Empty;
+        public string UserCity { get; set; } = String.Empty;
+        public string UserCountry { get; set; } = String.Empty;
+        public string UserAdress { get; set; } = String.Empty;
+        public string UserDateCreating { get; set; } = String.Empty;
+
+
+
+
+
+    }
+}

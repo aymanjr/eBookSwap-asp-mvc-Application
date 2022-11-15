936ac8e8fde6919abc0a054e3e658b1fc9a9d8a2
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-11-15 16:30:19 +0100

    adding controllers

---
+++ ebookSwapApllication/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using ebookSwapApllication.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ebookSwapApllication.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ebookSwapApllication/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using ebookSwapApllication.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ebookSwapApllication.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Users.ToList();
+            return View();
+        }
+    }
+}

495703186f3731ac220679982646daeca91fbf44
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-12-02 18:15:29 +0100

    CHANGES

--- ebookSwapApllication/Models/LoginViewModel.cs
+++ ebookSwapApllication/Models/LoginViewModel.cs
@@ -3,19 +3,44 @@ using System.Xml.Linq;
 
 namespace ebookSwapApllication.Models
 {
-    public class LoginViewModel
+    public class LoginSignupViewModel
     {
-        [Required]
-        [Display(Name = "Username")]
-        public string Username { get; set; }
+        public int UserId { get; set; }
+        [Display(Name = "UserName")]
+        [Required(ErrorMessage = "username is required")]
+        public string UserName { get; set; } = String.Empty;
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "FullName is required")]
 
- 
-        [Required]
-        [DataType(DataType.Password)]
+        public string UserFullName { get; set; } = String.Empty;
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required")]
+
+        public string UserEmail { get; set; } = String.Empty;
         [Display(Name = "Password")]
-        public string Password { get; set; }
+        [Required(ErrorMessage = "Password is required")]
 
+        public string UserPassword { get; set; } = String.Empty;
+        public string UserConfirmPassword { get; set; } = String.Empty;
 
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Phone Number is required")]
+
+        public string Userphonenumber { get; set; } = String.Empty;
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "City is required")]
+
+        public string UserCity { get; set; } = String.Empty;
+        [Display(Name = "Country")]
+
+        public string UserCountry { get; set; } = String.Empty;
+        [Display(Name = "Adress")]
+        [Required(ErrorMessage = "Adress is required")]
+
+        public string UserAdress { get; set; } = String.Empty;
+        [Display(Name = "Date Creation")]
+
+        public string UserDateCreating { get; set; } = String.Empty;
 
 
         public bool IsActive { get; set; }
